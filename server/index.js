f05f210397b98f6a1c6c440d2c7465655338f675
david-osas <azamegbeosarumense@gmail.com>
2020-10-07 11:32:20 +0100

    Add initial code

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+
+
+app.post('/server/send-problem', (req,res) => {
+  let {department, courseTitle, year, topic, problem} = req.body
+
+
+})
+
+app.post('/server/signin', (req,res) => {
+  let {email, password} = req.body
+
+})
+
+app.post('/server/signup', (req,res) => {
+  let {username, email, password} = req.body
+
+})
+
+app.delete('/server/logout', (req,res) => {
+
+})
+
+
+
+app.listen(5000, () => {
+  console.log('server started on port 5000')
+})
