4e5753ce1c64982e992cc2381d7de696aacac585
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-10-07 08:34:25 -0400

    Add Header

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './Header';
+import Home from './Home';
+import Login from './Login';
+import Signup from './Signup';
 
 const App = () => {
     return (
-        <div>
-            APP
+        <div className="ui container">
+            <BrowserRouter>
+                <Header />
+                <Route path="/" exact component={Home} />
+                <Route path="/login" exact component={Login} />
+                <Route path="/signup" exact component={Signup} />
+            </BrowserRouter>
+
         </div>
     );
 };
