1313fbe473a49a22b6b7befc47e6b528e84fcb21
david-osas <azamegbeosarumense@gmail.com>
2020-10-07 11:21:02 +0100

    Add authentication code

--- server/index.js
+++ server/index.js
@@ -2,6 +2,22 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.json())
+
+app.post('/server/signin', (req,res) => {
+  let {email, password} = req.body
+
+})
+
+app.post('/server/signup', (req,res) => {
+  let {username, email, password, department} = req.body
+
+})
+
+app.delete('/server/logout', (req,res) => {
+
+})
+
 
 
 
