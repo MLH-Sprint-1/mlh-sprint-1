abe4d560baca33126c464c239a270969eca3eb60
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-10-07 10:41:51 -0400

    Create Initial Login/Signup Forms

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Header = () => {
+const Header = ({ loginStatus }) => {
+
+    // if user is logged in, should display 'Logout'
+    // else, should show 'Login' and 'Signup'
+
+
     return (
         <div className="ui secondary pointing menu">
             <Link to='/' className="item">
