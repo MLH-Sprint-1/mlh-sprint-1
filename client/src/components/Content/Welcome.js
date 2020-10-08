66a7e1b891affde0cff8fafb5c059125175d6171
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-10-08 10:28:32 -0400

    Add Welcome Message Cards Starter Code

--- client/src/components/Content/Welcome.js
+++ client/src/components/Content/Welcome.js
@@ -3,7 +3,35 @@ import React from 'react';
 const Welcome = () => {
     return (
         <div>
-            WELCOME TO THE SITE
+            How We Benefit:
+            <div className="ui cards">
+                <div className="card">
+                    <div className="content">
+                        <div className="header">
+                            Students
+                        </div>
+                        <div className="description">
+                            <ul>
+                                <li>reason 1</li>
+                                <li>reason 2</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="content">
+                        <div className="header">
+                            Professors
+                        </div>
+                        <div className="description">
+                            <ul>
+                                <li>reason 1</li>
+                                <li>reason 2</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
