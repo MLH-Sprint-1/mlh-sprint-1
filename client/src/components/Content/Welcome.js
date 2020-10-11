2e097cd5545716493c21bc62553cd125f283a90c
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-10-11 08:04:05 -0400

    Change Welcome Layout to Divider

--- client/src/components/Content/Welcome.js
+++ client/src/components/Content/Welcome.js
@@ -1,38 +1,57 @@
 import React from 'react';
 
 const Welcome = () => {
+
+    // creating content for student/professor benefits
+    const benefits = [
+        {
+           'type': 'Students',
+           'benefits': [
+            'Ask questions and see questions from fellow peers',
+            ' Answer questions to receive credit towards your grade'
+            ]
+        },
+        {
+            'type': 'Professors',
+            'benefits': [
+                'With students helping other students, your demand for office hours will lessen so you can focus on your other courses or projects',
+                'See student solutions to problems so you can refine subsequent lectures',
+                'Increase demand for your course with student incentive',
+                'See data about your lectures such as who is asking questions about which points in the lecture',
+                'Identify potential teaching assistants among your students'
+            ]
+        }
+    ];
+
+    // function to iterate through array of benefits and create divs
+    const benefitDivs = (benefitsArr) => {
+        return benefitsArr.map(elem => {
+            return <div className="item">{elem}</div>
+        });
+    };
+
+    // create divs for each type/benefit in array (of objects)
+    const benefitsElem = benefits.map(benefit => {
+        return (
+            <div className="column">
+                <h3 className="ui center aligned header">{benefit['type']}</h3>
+                <div className="ui clearing divider"></div>
+                <div className="ui bulleted list">
+                    {benefitDivs(benefit['benefits'])}
+                </div>
+            </div>
+        );
+    });
+
     return (
         <div>
-            How We Help:
-            <div className="ui cards">
-                <div className="card">
-                    <div className="content">
-                        <div className="header">
-                            Students
-                        </div>
-                        <div className="description">
-                            <ul>
-                                <li>Ask/Answer questions about your lectures and see related questions from fellow students</li>
-                                <li>Earn credit for helping your peers and redeem for grade improvement</li>
-                            </ul>
-                        </div>
-                    </div>
+            <h2 className="ui center aligned header">How We Help:</h2>
+            <div className="ui segment">
+                <div className="ui two column very relaxed grid">
+                    {benefitsElem}
                 </div>
-                <div className="card">
-                    <div className="content">
-                        <div className="header">
-                            Professors
-                        </div>
-                        <div className="description">
-                            <ul>
-                                <li>With students helping other students, your demand for office hours will lessen so you can focus on your other courses or projects</li>
-                                <li>See student solutions to problems so you can refine subsequent lectures</li>
-                                <li>Increase demand for your course with student incentive</li>
-                                <li>See data about your lectures such as who is asking questions about which points in the lecture</li>
-                                <li>Identify potential teaching assistants among your students</li>
-                            </ul>
-                        </div>
-                    </div>
+                <div className="ui vertical divider">
+                    and
                 </div>
             </div>
         </div>
