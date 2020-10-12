d6f2c457783b0cbe8342d4a007aa7bda923ece7b
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-10-12 19:52:44 -0400

    Make Cards from Questions

--- client/src/components/QuestionList.js
+++ client/src/components/QuestionList.js
@@ -11,17 +11,42 @@ function QuestionList(){
   }
   useEffect(() => {
     fetchData()
-  })
+  }, [])
 
   if(questions.length === 0){
-    return <h1>No data to show yet</h1>
+    return (
+      <div className="ui segment">
+        <div className="ui active inverted dimmer">
+          <div className="ui text loader">Loading</div>
+        </div>
+      </div>
+    )
   }
 
+  const questionCards = questions.map((question, index) => {
+    const { request, request_date, topic } = question;
+    return (
+      <div key={index} className="card">
+        <div className="content">
+          <div className="header">{topic}</div>
+          <div className="description">{request}</div>
+          <div className="extra content">{request_date}</div>
+        </div>
+      </div>
+    )
+  });
+
   return (
-    <div className='row row-cols-4'>
-      {questions.map((item) => <QuestionListItem title={item.topic}  text={item.request}/> )}
+    <div className="ui cards">
+      {questionCards}
     </div>
   )
+
+  //return (
+  //  <div className='row row-cols-4'>
+  //    {questions.map((item) => <QuestionListItem title={item.topic}  text={item.request}/> )}
+  //  </div>
+  //)
 }
 
 export default QuestionList
